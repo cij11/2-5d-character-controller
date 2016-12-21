aba1bd8a4fc5ed3cdc3ca05fd6982a18c1c2ba43
cij11 <cij11@uclive.ac.nz>
2016-12-21 20:53:26 +1300

    Added basic weapon and basic projectile. Takes aiming vector from aimingcontroller, and instantiates a projectile gameobject. Currently, both weapon and aiming take input directly, instead of from the input component attached to the rigidbody.

---
+++ 2-5d-character-controller/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+
+	public GameObject projectileGO;
+
+	AimingController aimingController;
+
+	float muzzleSpeed = 20f;
+
+	// Use this for initialization
+	void Start () {
+		aimingController = this.transform.parent.Find("Aiming").GetComponent<AimingController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonUp("Fire1")){
+			Vector3 aimingVector = aimingController.GetAimingVector();
+			GameObject newProjectile = (GameObject)Instantiate(projectileGO, this.transform.position + aimingVector, Quaternion.identity);
+			newProjectile.GetComponent<Rigidbody>().velocity = aimingVector  * muzzleSpeed;
+		}
+	}
+}
