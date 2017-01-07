29f16054ef59f2663e3a785226e338674c9c0069
cij11 <cij11@uclive.ac.nz>
2017-01-07 14:32:36 +1300

    Added radial sensor. Public methods to get if player is within sensor range, and get the player gameObject (or null).

---
+++ 2-5d-character-controller/Assets/Scripts/RadusObjectSensor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadusObjectSensor : MonoBehaviour
+{
+
+    float sensorRadius = 10f;
+
+    bool playerDetected = false;
+    UpdateTimer updateTimer;
+    Rigidbody body;
+    public LayerMask collisionMask;
+    Collider[] hitColliders;
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody>() as Rigidbody;
+        updateTimer = new UpdateTimer(10);
+        hitColliders = new Collider[0];
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (updateTimer.TryUpdateThisTick())
+        {
+            hitColliders = Physics.OverlapSphere(body.transform.position, sensorRadius, collisionMask);
+        }
+    }
+
+    public bool PlayerDetected()
+    {
+        GameObject playerObject = GetPlayerGameObject();
+		if (playerObject != null){
+			return true;
+		}
+		else{
+			return false;
+		}
+    }
+
+    public GameObject GetPlayerGameObject()
+    {
+		GameObject playerObject = null;
+		foreach (Collider collider in hitColliders)
+        {
+            if (collider.tag == "Player")
+            {
+                playerObject = collider.gameObject;
+            }
+        }
+		return playerObject;
+    }
+}
