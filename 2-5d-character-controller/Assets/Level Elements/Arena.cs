cddf3cda6f28bd5d65b7eea68333e47d16c78d88
cij11 <cij11@uclive.ac.nz>
2017-02-27 19:52:16 +1300

    Added Arena prefab and script. An an Arena gameObject will store references to any Spawners which are children of its transform. It checks if all of its children are exhausted. If so, it currently prints 'Arena Complete'. In future, it will unlock the next area or activate the next arena, or trigger level completion if it is the final arena in a level.

---
+++ 2-5d-character-controller/Assets/Level Elements/Arena.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour {
+
+	bool spawnersExhausted = false;
+
+	Spawner[] spawners;
+
+	// Use this for initialization
+	void Start () {
+		StoreSpawners ();
+	}
+
+	void StoreSpawners(){
+		spawners = GetComponentsInChildren<Spawner> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!spawnersExhausted) {
+			if (CheckSpawnersExhausted ()) {
+				spawnersExhausted = true;
+				print ("Arena complete");
+			}
+		}
+	}
+
+	bool CheckSpawnersExhausted(){
+		bool exhausted = true;
+		foreach (Spawner spawner in spawners) {
+			//If any spawners are not exhausted, then the arena is not complete
+			if (!spawner.GetIsSpawnerExhausted ()) {
+				exhausted = false;
+			}
+		}
+		return exhausted;
+	}
+}
