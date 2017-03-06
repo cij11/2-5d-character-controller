01e20f4febbbddc74c5f4bbd7f012982af8ae219
cij11 <cij11@uclive.ac.nz>
2017-03-06 13:59:11 +1300

    Started work on system to generate labyrinth of rooms inside a circular planet. Simple maze generation starts with one room. Randomly selects a direction, tries to build a room there if a room does not already exist, then adds that list to a pool of rooms. Algorithm continues until all directions have been explored from all rooms, eg, until unable to place more rooms.

---
+++ 2-5d-character-controller/Assets/Labyrinth/LabyrinthBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthBuilder : MonoBehaviour {
+	public float WorldRadius = 100f;
+	public float roomDiameter = 5f;
+	public float roomSeparation = 2f;
+	public GameObject roomBoundryPrefab;
+
+	List<LabyrinthRoom> unfinishedRoomList;
+	List<LabyrinthRoom> finishedRoomList;
+
+	// Use this for initialization
+	void Start () {
+		unfinishedRoomList = new List<LabyrinthRoom> ();
+		finishedRoomList = new List<LabyrinthRoom> ();
+		BuildLabyrinth ();
+	}
+
+	//Build a labyrinth room by room. Pick a random room, and explore a random direction from that room.
+	//Once every direction has been explored from a room (and either been extended, or discarded), remove
+	//that room from the pool of unfinished rooms.
+	void BuildLabyrinth(){
+		//Build a starting room
+		BuildRoom (new Vector3 (1f, 0f, 0f));
+
+		//While there are unfinished rooms
+		while (unfinishedRoomList.Count != 0 && finishedRoomList.Count < 20) {
+			int nextRoomIndex = Random.Range (0, unfinishedRoomList.Count);
+			LabyrinthRoom testingRoom = unfinishedRoomList [nextRoomIndex];
+			print ("Testing room number " + nextRoomIndex.ToString());
+			//If this room has remaining directions, pick a random direction to try and extend
+			if (testingRoom.HasUnexploredDirection ()) {
+				MovementDirection exploringDirection = testingRoom.PopRandomUnexploredDirection ();
+				print ("Testing direction " + exploringDirection.ToString ());
+				Vector3 exploringVector = testingRoom.DirectionToVector (exploringDirection);
+
+				Vector3 proposedLocation = testingRoom.GetPosition () + exploringVector * roomSeparation;
+				BuildRoom (proposedLocation);
+
+			} else { //Otherwise, add to finished room list, and remove from unfinished room list.
+				finishedRoomList.Add (testingRoom);
+				unfinishedRoomList.RemoveAt (nextRoomIndex);
+			}
+		}
+	}
+
+	void BuildRoom(Vector3 location){
+		LabyrinthRoom newRoom = new LabyrinthRoom (location);
+		unfinishedRoomList.Add (newRoom);
+		GameObject newRoomBoundry = Instantiate (roomBoundryPrefab, location, Quaternion.identity);
+	}
+}
---
+++ 2-5d-character-controller/Assets/Labyrinth/LabyrinthRoom.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthRoom {
+	Vector3 position;
+	List<MovementDirection> corridorDirections;
+
+	public LabyrinthRoom(Vector3 position){
+		this.position = position;
+		corridorDirections = new List<MovementDirection> ();
+		corridorDirections.Add (MovementDirection.RIGHT);
+		corridorDirections.Add (MovementDirection.LEFT);
+		corridorDirections.Add (MovementDirection.UP);
+		corridorDirections.Add (MovementDirection.DOWN);
+	}
+
+	public bool HasUnexploredDirection(){
+		if (corridorDirections.Count != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	//Pop a random direction from the set of 4 possibilities and return it
+	public MovementDirection PopRandomUnexploredDirection(){
+		int randomIndex = Random.Range (0, corridorDirections.Count);
+		MovementDirection selectedDirection = corridorDirections [randomIndex];
+		corridorDirections.RemoveAt (randomIndex);
+		return selectedDirection;
+	}
+
+	public Vector3 GetPosition(){
+		return position;
+	}
+
+	public Vector3 DirectionToVector(MovementDirection direction){
+		Vector3 vec = new Vector3 (1f, 0f, 0f);
+		Vector3 directionFromCenter = position;
+		directionFromCenter.Normalize ();
+		Vector3 tangentVec = new Vector3 (directionFromCenter.y, -directionFromCenter.x, 0);
+
+		switch (direction) {
+		case MovementDirection.RIGHT:
+			vec = tangentVec;
+			break;
+		case MovementDirection.LEFT:
+			vec = -tangentVec;
+			break;
+		case MovementDirection.UP:
+			vec = directionFromCenter;
+			break;
+		case MovementDirection.DOWN:
+			vec = -directionFromCenter;
+			break;
+		}
+		return vec;
+	}
+}
