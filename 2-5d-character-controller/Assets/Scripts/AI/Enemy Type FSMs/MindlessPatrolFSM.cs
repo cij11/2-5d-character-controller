53dbddc5b1111fdb042131777996bf36c2fd1620
cij11 <cij11@uclive.ac.nz>
2017-04-24 22:25:56 +1200

    Added an enemy that jumps over the player to the bunker arena scene. Also added an enemy that patrols left and right with a cattle prod (does damage, defects projectiles).

---
+++ 2-5d-character-controller/Assets/Scripts/AI/Enemy Type FSMs/MindlessPatrolFSM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MindlessPatrolFSM : FSMLoader {
+
+	protected override void SeriallyLoadStates(){
+
+		AddState ("patrol", Action.IDLE);
+		AddTransistion ("move_forward", Condition.TIMER, 1, true);
+
+		AddState ("move_forward", Action.MOVE_FORWARD);
+		AddTransistion ("change_direction", Condition.CLIFF_FORWARD, 1, true);
+
+		AddState ("change_direction", Action.CHANGE_DIRECTION);
+		AddTransistion ("move_forward", Condition.FRAMES, 1, true);
+	}
+}
