1e4a20bd87b54994cce98f6ede79d99fb0bba8a4
cij11 <cij11@uclive.ac.nz>
2017-04-25 13:27:33 +1200

    Added randomly bouncing enemy.

---
+++ 2-5d-character-controller/Assets/Scripts/AI/Enemy Type FSMs/RandomBouncerFSM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBouncerFSM : FSMLoader {
+
+
+	protected override void SeriallyLoadStates(){
+		AddState ("float_randomly", Action.RUN_SUB_FSM, "pick_random_rose");
+		AddTransistion ("attack", Condition.TARGET_IN_RADIUS, 5, true);
+
+		AddState ("attack", Action.RUN_SUB_FSM, "aim_target");
+		AddTransistion ("float_randomly", Condition.TARGET_IN_RADIUS, 5, false);
+
+		AddState ("pick_random_rose", Action.PICK_RANDOM_ROSE_DIRECTION);
+		AddTransistion ("move_rose_direction", Condition.FRAMES, 2, true);
+
+		AddState ("move_rose_direction", Action.MOVE_ROSE_DIRECTION);
+		AddTransistion ("pick_random_rose", Condition.TIMER, 3, true);
+
+		AddState ("aim_target", Action.AIM_TARGET);
+		AddTransistion ("shoot_target", Condition.TIMER, 2, true);
+
+		AddState ("shoot_target", Action.RELEASE_FIRE_TARGET);
+		AddTransistion ("aim_target", Condition.TIMER, 0.1f, true);
+	}
+
+	protected override void ChooseStartingState(){
+		startingStateName = "float_randomly";
+	}
+}
+	
