6be5332ef3ca9d3310ee0b75c87edbb2ef7f5258
cij11 <cij11@uclive.ac.nz>
2017-04-27 16:56:35 +1200

    Display instructions on first screen. Will move to main menu.

--- 2-5d-character-controller/Assets/Level Elements/Instructions.cs
+++ 2-5d-character-controller/Assets/Level Elements/Instructions.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Instructions : MonoBehaviour {
 	float fadeAlpha = 1.0f;
+	public float persistTime = 8;
 	public string text = "Placeholder";
+
+	public bool bottomOfScreen = false;
 
 	// Use this for initialization
 	void Start () {
-		Invoke ("FadeOut", 8);
+		Invoke ("FadeOut", persistTime);
 	}
 		
 	void OnGUI()
@@ -18,10 +21,19 @@ public class Instructions : MonoBehaviour {
 		GUIStyle style = new GUIStyle();
 
 		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperCenter;
+		if(!bottomOfScreen)
+			style.alignment = TextAnchor.UpperCenter;
+		else
+			style.alignment = TextAnchor.UpperCenter;
+		
 		style.fontSize = h * 8 / 100;
 		style.normal.textColor = new Color (1.0f, 1.0f, 1.0f, fadeAlpha);
-		GUI.Label(rect, "\n\n\n" + text, style);
+
+		if (!bottomOfScreen) {
+			GUI.Label (rect, "\n\n\n" + text, style);
+		} else {
+			GUI.Label (rect, "\n\n\n\n\n\n\n WASD to move. \n Space to jump and wall-jump  \n J to aim and fire \n K to pickup and cycle weapons", style);
+		}
 	}
 
 	void FadeOut(){
