88a5c57255ac7c6876c6a1ec14b0ca23554056c6
cij11 <cij11@uclive.ac.nz>
2017-04-29 21:51:27 +1200

    Added fade in on level load.

---
+++ 2-5d-character-controller/Assets/Level Elements/Fader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour {
+
+	Renderer rend;
+	float timer;
+	public float period = 2f;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> () as Renderer;
+		timer = period;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float percentage = 0;
+		if (timer > 0) {
+			timer -= Time.deltaTime;
+			percentage = timer / period;
+		} else {
+			Destroy (this.gameObject);
+		}
+		rend.material.color = new Color (rend.material.color.r, rend.material.color.g, rend.material.color.b, percentage);
+
+	}
+}
